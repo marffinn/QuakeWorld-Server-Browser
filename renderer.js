7833b2ce640fd101393d63708c5278ab5f76f682
Tomasz Drozdowski <marffinn@gmail.com>
2024-06-17 22:55:21 +0200

    w

--- renderer.js
+++ renderer.js
@@ -48,13 +48,12 @@ let updateFromMaster = () => {
     let m = addZero(d.getMinutes());
     let resultInfo = `Last refresh: ${h}:${m}`;
     $(".progress_text").html(resultInfo);
-    // progressBar.fadeOut(500)
   });
 };
 
 let readServers = () => {
   $(".appServerList").empty();
-  // // .\assets\qstat.exe -f .\assets\hos.txt -nh -ne -R -P -u -sort n  -json -of .\assets\cacheservers.json
+  // .\assets\qstat.exe -f .\assets\hos.txt -nh -ne -R -P -u -sort n  -json -of .\assets\cacheservers.json
   let ss = spawn(`assets/qstat.exe`, [
     "-f",
     "assets/hosts.txt",
@@ -68,28 +67,30 @@ let readServers = () => {
     "-json",
     "-of",
     `assets/cacheservers.json`,
-  ]);
-  //   let rawdata = fs.readFileSync(`assets/servers.json`);
-  //   let serverList = JSON.parse(rawdata);
-  //   for (let s in serverList) {
-  //     if (
-  //       serverList[s].ping >= 120 ||
-  //       serverList[s].map === undefined ||
-  //       serverList[s].map === "?" ||
-  //       serverList[s].numplayers == "0"
-  //     )
-  //       continue;
-  //     else {
-  //       let oneServerPrepare = `<tr href="${serverList[s].address}" data-name="${serverList[s].name}" data-ping="${serverList[s].ping}" data-playerno="${serverList[s].numplayers}">
-  //                     <th class="serverName text-truncate"><a href="${serverList[s].address}">${serverList[s].name}</a></th>
-  //                     <th class="serverPing">${serverList[s].ping}</th>
-  //                     <th class="serverMap">${serverList[s].map}</th>
-  //                     <th class="serverPlayers">${serverList[s].numplayers}/${serverList[s].maxplayers}</th>
-  //                 </tr>`;
-  //       $(".appServerList").append(oneServerPrepare);
-  //     }
-  //   }
+  ]).on("close", () => {
+    let rawdata = fs.readFileSync(`assets/cacheservers.json`);
+    let serverList = JSON.parse(rawdata);
+    for (let s in serverList) {
+      if (
+        serverList[s].ping >= 120 ||
+        serverList[s].map === undefined ||
+        serverList[s].map === "?" ||
+        serverList[s].numplayers == "0"
+      )
+        continue;
+      else {
+        let oneServerPrepare = `<tr href="${serverList[s].address}" data-name="${serverList[s].name}" data-ping="${serverList[s].ping}" data-playerno="${serverList[s].numplayers}">
+                      <th class="serverName text-truncate">${serverList[s].name}</th>
+                      <th class="serverPing">${serverList[s].ping}</th>
+                      <th class="serverMap">${serverList[s].map}</th>
+                      <th class="serverPlayers">${serverList[s].numplayers}/${serverList[s].maxplayers}</th>
+                  </tr>`;
+        $(".appServerList").append(oneServerPrepare);
+      }
+    }
+  });
 };
+
 $(".btn_update_masters").on("click", updateFromMaster);
 $(".btn_refresh_servers").on("click", readServers);
 
@@ -97,5 +98,3 @@ $("body").on("click", ".appServerList tr", function (e) {
   e.preventDefault();
   alert("siemka");
 });
-
-//
