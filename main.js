8e0f33dc057889f92a5b85ac80ee4394ba2c2fc7
Tomasz Drozdowski <marffinn@gmail.com>
2024-06-18 17:26:12 +0200

    dc

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
-const {ipcMain} = require('electron')
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+const { ipcMain } = require("electron");
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 500,
     frame: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, "preload.js"),
     },
     autoHideMenuBar: true,
-  })
+  });
 
-  mainWindow.loadFile('index.html')
-  win.webContents.openDevTools()
-}
+  mainWindow.loadFile("index.html");
+  win.webContents.openDevTools();
+};
 
 app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
