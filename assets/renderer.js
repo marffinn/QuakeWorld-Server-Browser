04a836f452264e1d7993365f44f908826c5f8406
Tomasz Drozdowski <marffinn@gmail.com>
2024-06-29 13:19:33 +0200

    fn merge

--- assets/renderer.js
+++ assets/renderer.js
@@ -7,14 +7,14 @@ const { log } = require("console");
 
 const QSTAT_PATH = require("path").join(__dirname, "assets/qstat.exe");
 let progressBar = $(".progress_bar_percentage");
+let addZero = (i) => {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+};
 
 let updateFromMaster = () => {
-  let addZero = (i) => {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  };
   progressBar.show();
 
   const ls = spawn(`assets/qstat.exe`, [
@@ -69,29 +69,35 @@ let readServers = () => {
     $(".appServerList").empty();
     let rawdata = fs.readFileSync(`assets/cacheservers.json`);
     let serverList = JSON.parse(rawdata);
-    for (let s in serverList) {
-      if (
-        serverList[s].map === undefined ||
-        serverList[s].map === "?" ||
-        serverList[s].numplayers == "0"
-      )
-        continue;
-      else {
-        let oneServerPrepare = `<tr href="${serverList[s].address}" data-name="${serverList[s].name}" data-ping="${serverList[s].ping}" data-playerno="${serverList[s].numplayers}">
-                      <th class="serverName text-truncate">${serverList[s].name}</th>
-                      <th class="serverPing">${serverList[s].ping}</th>
-                      <th class="serverMap">${serverList[s].map}</th>
-                      <th class="serverPlayers">${serverList[s].numplayers}/${serverList[s].maxplayers}</th>
-                  </tr>`;
-        $(".appServerList").append(oneServerPrepare);
-      }
-    }
+    cardRender(serverList);
     $(".btn_refresh_servers").prop("disabled", false).removeClass("disabled");
   });
 };
 
-$(".btn_update_masters").on("click", updateFromMaster);
-$(".btn_refresh_servers").on("click", readServers);
+let cardRender = (data) => {
+  for (let s in data) {
+    if (data[s].map === undefined || data[s].map === "?") continue;
+    else {
+      let oneServerPrepare = `
+      <div class="server-card" href="${data[s].address}" data-name="${data[s].name}">
+        <div class="server-card-bg">
+          <img src="assets/mapshots/${data[s].map}.jpg" alt="${data[s].map}"/>
+          <div class="serverPing">${data[s].ping}</div>
+          <div class="serverMap">${data[s].map}</div>
+        </div>
+        <div class="serverName"> ${data[s].name} </div>
+        <div class="serverPlayers">${data[s].numplayers}/${data[s].maxplayers}</div>
+      </div>`;
+      $(".appServerList").append(oneServerPrepare);
+    }
+  }
+};
+
+let onAppLoad = () => {
+  let rawdata = fs.readFileSync(`assets/cacheservers.json`);
+  let serverList = JSON.parse(rawdata);
+  cardRender(serverList);
+};
 
 $("body").on("click", ".server-card", function (e) {
   e.preventDefault();
@@ -111,24 +117,7 @@ $("body").on("click", ".server-card", function (e) {
   );
 });
 
-let onAppLoad = () => {
-  let rawdata = fs.readFileSync(`assets/cacheservers.json`);
-  let serverList = JSON.parse(rawdata);
-  for (let s in serverList) {
-    if (serverList[s].map === undefined || serverList[s].map === "?") continue;
-    else {
-      let oneServerPrepare = `<div class="server-card" href="${serverList[s].address}" data-name="${serverList[s].name}">
-              <div class="server-card-bg">
-                <img src="assets/mapshots/${serverList[s].map}.jpg" alt="${serverList[s].map}"/>
-                <div class="serverPing">${serverList[s].ping}</div>
-                <div class="serverMap">${serverList[s].map}</div>
-              </div>
-              <div class="serverName"> ${serverList[s].name} </div>
-              <div class="serverPlayers">${serverList[s].numplayers}/${serverList[s].maxplayers}</div>
-          </div>`;
-      $(".appServerList").append(oneServerPrepare);
-    }
-  }
-};
+$(".btn_update_masters").on("click", updateFromMaster);
+$(".btn_refresh_servers").on("click", readServers);
 
 onAppLoad();
